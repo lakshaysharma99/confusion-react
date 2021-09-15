2d0a1f2fdcc162b6c65fc0284479dcb8ed15744f
unknown <lakshays206@gmail.com>
2021-09-15 20:51:59 +0530

    spa-2

--- src/components/DishdetailComponent.js
+++ src/components/DishdetailComponent.js
@@ -1,7 +1,6 @@
 import React from "react";
-import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
-
-
+import { Card, CardImg, CardText, CardBody, Breadcrumb, BreadcrumbItem, CardTitle } from 'reactstrap';
+import {Link} from 'react-router-dom';
 
     // componentDidMount(){
     //     console.log('Disdetail Component componentDidMount invoked');
@@ -12,7 +11,6 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     // }
 
     function RenderDish({dish}){
-        if (dish!=null){
             return(
                 <div className="col-12 col-md-5 m-1">
                     <Card>
@@ -24,25 +22,20 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
                     </Card>
                 </div>
             );
-        }
-        else{
-            return (
-                <div></div>
-            );
-        }
     }
 
-    function RenderComments({comments}){
+    function RenderComments({comments}) {
+        console.log(comments)
         if(comments != null){
             return(
                 <div className="col-12 col-md-5 m-1">
                     <h4>Comments</h4>
-                    <ul className="list-unstyled">
+                    <ul className="list-unstyled"> 
                         {comments.map((comment) => {
                             return(
-                                <li key={comment.id} className="list-unstyled">
+                                <li key={comment.id}>
                                     <p>{comment.comment}</p>
-                                    <p>-- {comment.author} ; {new Intl.DateTimeFormat('en-US', {year:"numeric", month: "short", day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
+                                    <p>-- {comment.author} , {new Intl.DateTimeFormat('en-US', {year:"numeric", month: "short", day:'2-digit'}).format(new Date(Date.parse(comment.date)))}</p>
                                 </li>
                             );
                         })}
@@ -58,20 +51,27 @@ import { Card, CardImg, CardText, CardBody, CardTitle } from 'reactstrap';
     }
 
     const DishDetail = (props) => {
+        if (props.dish!= null){
+            return (
+                <div className="container">
+                <div className="row">
+                    <Breadcrumb>
 
-        console.log('Disdetail Component render invoked');
-
-
-        const dish = props.dish
-
-
-        if (dish!= null){
-            return(
-                <div class="container">
-                    <div className="row">
-                        <RenderDish dish={dish}/>
-                        <RenderComments comments={dish.comments} />
-                    </div>
+                        <BreadcrumbItem><Link to="/menu">Menu</Link></BreadcrumbItem>
+                        <BreadcrumbItem active>{props.dish.name}</BreadcrumbItem>
+                    </Breadcrumb>
+                    <div className="col-12">
+                        <h3>{props.dish.name}</h3>
+                        <hr />
+                    </div>                
+                </div>
+                <div className="row">
+                    
+                        <RenderDish dish={props.dish} />
+                    
+                        <RenderComments comments={props.comments} />
+                    
+                </div>
                 </div>
             );
         }
